53db04f87ab7908c0203e8f57745d92fd86fe1ea
Pankaj Kumar <PANKAJKUMAR1767361@gmail.com>
2026-02-28 00:55:19 +0530

    daily update

---
+++ 03-JAVASCRIPT/Day02/basic.js
@@ -0,0 +1,13 @@
+let name = "Rohit";
+let age = 20;
+
+if(true){
+    let c = 90;
+}
+
+age = 30;
+console.log(c);      // ❌ ERROR
+console.log(name , age);
+
+const account = 1234;
+console.log(account);
